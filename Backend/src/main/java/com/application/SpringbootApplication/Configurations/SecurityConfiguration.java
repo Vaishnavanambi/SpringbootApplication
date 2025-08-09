25985bb5684a097d98de07f22ad596bea29feef3
Nambi <seenivasanv50@gmail.com>
2025-08-09 22:21:04 +0530

    moving file

--- Backend/src/main/java/com/application/SpringbootApplication/Configurations/SecurityConfiguration.java
+++ Backend/src/main/java/com/application/SpringbootApplication/Configurations/SecurityConfiguration.java
@@ -2,15 +2,18 @@ package com.application.SpringbootApplication.Configurations;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
-    /*@Bean
+    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
-        httpSecurity.authorizeHttpRequests(obj -> obj.requestMatchers("/api").hasAnyRole())
-    }*/
+        httpSecurity.authorizeHttpRequests(obj -> obj.requestMatchers("/api/register").permitAll().anyRequest().authenticated()).csrf(AbstractHttpConfigurer::disable).httpBasic(Customizer.withDefaults());
+        return httpSecurity.build();
+    }
 }
--- Backend/src/main/java/com/application/SpringbootApplication/Controllers/UserConfigController.java
+++ Backend/src/main/java/com/application/SpringbootApplication/Controllers/UserConfigController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 @RestController
 @RequestMapping("/api")
-public class TestController {
+public class UserConfigController {
 
     @GetMapping("/test")
     public ResponseEntity<String> testMethod(){
